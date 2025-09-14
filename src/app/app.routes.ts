6c8c219877f93c09e4b88b13cba1df12a89a72ba
Yousef <yousefibrahim9@outlook.com>
2025-09-14 11:44:54 +0300

    feat: add main layout, home page, and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {
+        path: 'home',
+        component: HomePageComponent,
+        title: 'Coffee Menu',
+      },
+    ],
+  },
+];
